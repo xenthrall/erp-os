ecdf1aef8e3eff0eb29bd664bc70e71768580777
xenthrall <xenthrall0@gmail.com>
2026-02-22 00:13:35 -0500

    feat: agrega Branding(Logo y Favicon), personaliza paleta de colores panel admin

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -11,7 +11,6 @@ use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Filament\Widgets\AccountWidget;
-use Filament\Widgets\FilamentInfoWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -28,9 +27,22 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->profile()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::hex('#00e676'),
+                'gray'    => Color::Zinc,
+                'danger'  => Color::Rose,
+                'info'    => Color::Blue,
+                'success' => Color::Emerald,
+                'warning' => Color::Orange,
             ])
+            //->font('Montserrat')
+            // 3. BRANDING (LOGO Y FAVICON)
+            ->brandLogo(asset('images/logo-operacion-sistemica-light.webp'))
+            ->darkModeBrandLogo(asset('images/logo-operacion-sistemica-dark.webp'))
+            ->brandLogoHeight('2.5rem') 
+            ->favicon(asset('favicon.svg'))
+
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
@@ -39,7 +51,6 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
                 AccountWidget::class,
-                FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
