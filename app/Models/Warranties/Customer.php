82e658a21486587d4c68e359676b0319faa2deb1
xenthrall <xenthrall0@gmail.com>
2026-02-27 11:24:38 -0500

    fix: Confiar en todos los proxies, evita error de contenido mixto en el navegador

--- app/Models/Warranties/Customer.php
+++ app/Models/Warranties/Customer.php
@@ -4,6 +4,8 @@ namespace App\Models\Warranties;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class Customer extends Model
 {
@@ -17,6 +19,14 @@ class Customer extends Model
         'is_active',
     ];
 
+    /**
+     * Relación Inversa Polimórfica: Un cliente tiene unas credenciales de usuario
+     */
+    public function user(): MorphOne
+    {
+        return $this->morphOne(User::class, 'userable');
+    }
+    
     public function warrantyRequests(): HasMany
     {
         return $this->hasMany(WarrantyRequest::class);
