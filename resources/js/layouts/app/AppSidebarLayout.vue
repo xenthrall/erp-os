9e69b4f6a65b7803f0df23f6e90d6f77f9a2b294
xenthrall <xenthrall0@gmail.com>
2026-03-01 18:32:45 -0500

    feat(customer:warranty): implementa notificacion flash

--- resources/js/layouts/app/AppSidebarLayout.vue
+++ resources/js/layouts/app/AppSidebarLayout.vue
@@ -3,6 +3,7 @@ import AppContent from '@/components/AppContent.vue';
 import AppShell from '@/components/AppShell.vue';
 import AppSidebar from '@/components/AppSidebar.vue';
 import AppSidebarHeader from '@/components/AppSidebarHeader.vue';
+import ToastNotifications from '@/components/ToastNotifications.vue';
 import type { BreadcrumbItem } from '@/types';
 
 type Props = {
@@ -22,4 +23,8 @@ withDefaults(defineProps<Props>(), {
             <slot />
         </AppContent>
     </AppShell>
+
+    <!-- Notificaciones globales (flash de Inertia) -->
+    <ToastNotifications />
 </template>
+
